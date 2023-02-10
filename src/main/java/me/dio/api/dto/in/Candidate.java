65bd5c2da48c1d78df8d4bf47eada8f38189ad56
Thiago Poiani <thpoiani@gmail.com>
2023-02-10 12:46:42 -0700

    REST CLIENT: Added Multiny dependencies

--- src/main/java/me/dio/api/dto/in/Candidate.java
+++ src/main/java/me/dio/api/dto/in/Candidate.java
@@ -1,7 +1,10 @@
 package me.dio.api.dto.in;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.Optional;
 
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record Candidate(String id,
                         Optional<String> photo,
                         String fullName,
