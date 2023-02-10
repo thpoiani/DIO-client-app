b5cdde9036a5aac818ab10b325f15406806f053c
Thiago Poiani <thpoiani@gmail.com>
2023-02-10 13:23:09 -0700

    SERVER SENT EVENTS: Stream with time internal ticks consuming Election Management

---
+++ src/main/java/me/dio/infrastructure/resources/ElectionResultsResource.java
@@ -0,0 +1,31 @@
+package me.dio.infrastructure.resources;
+
+import io.smallrye.mutiny.Multi;
+import me.dio.api.dto.in.Election;
+import me.dio.infrastructure.rest.RestElectionManagementService;
+import org.jboss.resteasy.reactive.RestStreamElementType;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import java.time.Duration;
+import java.util.List;
+
+@Path("/election-results")
+public class ElectionResultsResource {
+    private final RestElectionManagementService electionManagementService;
+
+    public ElectionResultsResource(RestElectionManagementService electionManagementService) {
+        this.electionManagementService = electionManagementService;
+    }
+
+    @GET
+    @RestStreamElementType(MediaType.APPLICATION_JSON)
+    public Multi<List<Election>> results() {
+        return Multi.createFrom()
+                .ticks()
+                .every(Duration.ofSeconds(60))
+                .onItem()
+                .transformToMultiAndMerge(n -> electionManagementService.getElections().toMulti());
+   }
+}
